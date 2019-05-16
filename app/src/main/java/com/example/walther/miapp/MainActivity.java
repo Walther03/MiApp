a6915f537559cd608fefbb5f982c524a9ce9dcae
Walther03 <waportuguezo@gmail.com>
2019-05-16 17:41:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/walther/miapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.walther.miapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button btnInicio;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnInicio = findViewById(R.id.btninicio);
+        btnInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Login.class));
+                finish();
+            }
+        });
+
+
+    }
+}
